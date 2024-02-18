45e3348b1ab0359f0025ddb112a6b8df1a3f8d54
Damien Sedgwick <damienksedgwick@gmail.com>
2024-02-18 15:38:19 +0000

    added my config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "css",
+      "diff",
+      "go",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "vim",
+      "yaml",
+    },
+  },
+}
